62aeb57ebb980c32e51f250a14ce2113ce7bd1be
parkbeomun <sos891214@gmail.com>
2019-11-26 11:38:46 +0900

    webpack 및 babel 설정, react 기본코드 작성

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,1 +1,9 @@
-console.log('Set up is continuing...');
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const title = 'React And SpringBoot Example';
+
+ReactDOM.render(
+<div>{title}</div>,
+document.getElementById('root')
+)
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -9,5 +9,18 @@ module.exports = {
     },
     devServer: {
         contentBase: './dist'
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['*','.js','.jsx']
     }
 }
